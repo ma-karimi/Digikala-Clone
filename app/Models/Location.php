ed67b57975126fca6e7e5fb9e51e77fc888be741
ma-karimi <karimi.mak@gmail.com>
2020-12-29 23:32:30 +0330

    +Added-Location +Refactor-Relations

---
+++ app/Models/Location.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'location', 'state', 'city',
+        'address', 'number', 'unit', 'zip_code'];
+
+    public function user()
+    {
+     $this->hasOne(User::class);
+    }
+}
---
+++ database/migrations/2020_12_29_195133_create_locations_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id');
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete();
+            $table->string('location');
+            $table->string('state');
+            $table->string('city');
+            $table->string('address');
+            $table->integer('number');
+            $table->integer('unit');
+            $table->integer('zip_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
