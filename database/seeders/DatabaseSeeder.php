428a7293a283da77e5ca3e7594862514697ebf02
ma-karimi <karimi.mak@gmail.com>
2020-12-30 13:57:57 +0330

    +Added-Role-Seeder *Refactor-User-Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        UserSeeder::class;
+        $this->call([
+        RoleSeeder::class,
+        UserSeeder::class,
+    ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,19 +15,20 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create(['name' => 'super-admin', 'email' => 'super-admin@example.com', 'number' => '09122717839', 'password'=> '123456']);
+        $pass = Hash::make(123456);
+        $user = User::create(['name' => 'super-admin', 'email' => 'super-admin@example.com', 'mobile' => '09122717839', 'password'=> $pass]);
         $user->assignRole('super-admin');
 
-        $user = User::create(['name' => 'admin', 'email' => 'admin@example.com', 'number' => '09122717849', 'password'=> '123456']);
+        $user = User::create(['name' => 'admin', 'email' => 'admin@example.com', 'mobile' => '09122717849', 'password'=> $pass]);
         $user->assignRole('admin');
 
-        $user = User::create(['name' => 'seller', 'email' => 'seller@example.com', 'number' => '09122717859', 'password'=> '123456']);
+        $user = User::create(['name' => 'seller', 'email' => 'seller@example.com', 'mobile' => '09122717859', 'password'=> $pass]);
         $user->assignRole('seller');
 
-        $user = User::create(['name' => 'writer', 'email' => 'writer@example.com', 'number' => '09122717869', 'password'=> '123456']);
+        $user = User::create(['name' => 'writer', 'email' => 'writer@example.com', 'mobile' => '09122717869', 'password'=> $pass]);
         $user->assignRole('writer');
 
-        $user = User::create(['name' => 'user', 'email' => 'user@example.com', 'number' => '09122717879', 'password'=> '123456']);
+        $user = User::create(['name' => 'user', 'email' => 'user@example.com', 'mobile' => '09122717879', 'password'=> $pass]);
         $user->assignRole('user');
 
     }
